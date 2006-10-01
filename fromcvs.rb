b05d0a75b1578667041f71ac57742256240ae087
Simon 'corecode' Schubert <corecode@fs.ei.tum.de>
2006-10-01 22:45:46 +0200

    big whoopin' change.  functionality good, speed bad...

--- fromcvs.rb
+++ fromcvs.rb
@@ -5,7 +5,7 @@ require 'md5'
 
 class RCSFile::Rev
   attr_accessor :file, :syms, :author, :branches, :state, :rev, :next
-  attr_accessor :action
+  attr_accessor :action, :link, :branch_from
 
   # we sort revs on branch, author, log, date
   def <=>(rhs)
@@ -53,43 +53,39 @@ class RCSFile
       self.symbols.each_pair do |k, v|
 	f = v.split('.')
 	f.delete_at(-2) if f[-2] == '0'
-	next unless f.length == 3
-	@sym_rev[f] = [] unless @sym_rev[f]
+	next unless f.length % 2 == 1
+	@sym_rev[f] ||= []
 	@sym_rev[f].push(k).sort!
       end
     end
 
     branch = rev.split('.')[0..-2]
-    if @sym_rev.key?(branch)
-      return @sym_rev[branch]
-    else
-      return []
-    end
+    return @sym_rev[branch] || []
   end
 end
 
 
 class Repo
   class Set < Array
-    attr_accessor :author, :date
+    attr_accessor :author, :date, :ignore, :branch_from
 
-    def <<(rev)
-      super
+    def extract_data
+      @author = self[0].author
+      @date = self[0].date
+      @ignore = true
 
-      if rev.syms
+      bl = -1
+
+      each do |rev|
 	@branches ||= rev.syms
 	@branches &= rev.syms
+        if rev.branch_level > bl
+          @branch_from = rev.branch_from
+        end
+        @ignore &&= rev.action == :ignore
       end
-      unless @author
-	@author = rev.author
-	@date = rev.date
-      end
-    end
 
-    def push(*args)
-      for a in args
-	self << a
-      end
+      sort! {|a,b| a.file <=> b.file}
     end
 
     def branch
@@ -137,6 +133,8 @@ class Repo
       rh = {}
       nf = _normalize_path(f)
       RCSFile.open(f) do |rf|
+        trunkrev = nil    # "rev 1.2", when the vendor branch was overwritten
+
 	rf.each_value do |rev|
 	  # we need to record branch starts so that we can generate
 	  # the correct "inverse" next pointer for a later cvs diff
@@ -162,40 +160,59 @@ class Repo
 	  rev.rev = norm_h[rev.rev] ||= rev.rev
 	  rev.next = norm_h[rev.next] ||= rev.next
 	  rev.state = rev.state.intern
+
+          # the quest for trunkrev only happens on trunk (duh)
+          if rev.branch_level == 0
+            if rev.rev != '1.1'
+              trunkrev = rev if not trunkrev or trunkrev.date > rev.date
+            end
+          end
 	end
 
-        # if we are on a branch, ignore trunk, as it is unnamed
-        if rf.branch
-          branchpos = rf.branch[/^\d+\.\d+/]
-          rev = rh[rf.head]
-          while rev.rev != branchpos
-            rev.action = :ignore
-            rev = rh[rev.next]
-          end
-        end
+        # What we need to do to massage the revs correctly:
+        # * branch versions need action "branch" and a name assigned
+        # * branch versions without a named branch need to be ignored
+        # * if the first rev on a level 1-branch/1.1 is "dead", ignore this rev
+        # * if we are on a branch
+        #   - all trunk revs *above* the branch point are unnamed and
+        #     need to be ignored
+        #   - all main branch revisions need to be merged
+        #   - all branch revisions leading to the main branch need to be merged
+        # * if there is a vendor branch
+        #   - all vendor revisions older than "rev 1.2" need to be merged
+        #   - rev 1.1 needs to be ignored if it happened at the vendor branch time
+        #
 
         # special verndor branch handling:
         if rh.has_key?('1.1.1.1')
-          # find out if the file was taken off the vendor branch
-          trunkdate = nil
-          nomerge_vendor = rf.branch != nil && rf.branch != '1.1.1'
-          if rf.branch != '1.1.1'
-            rev = rh[rf.head]
-            while rev.rev != '1.1'
-              trunkdate = rev.date
-              rev = rh[rev.next]
-            end
+          # If the file was added only on the vendor branch, 1.2 is "dead", so ignore
+          # those revs completely
+          if trunkrev and trunkrev.next == '1.1' and
+                trunkrev.state == :dead and trunkrev.date == rh['1.1'].date
+            trunkrev.action = :ignore
+            rh['1.1'].action = :ignore
+          end
+
+          if rf.branch == '1.1.1'
+            trunkrev = nil
+          end
+
+          # some imports are without vendor symbol.  just fake one up then
+          vendor_sym = []
+          if rf.branch_syms_of('1.1.1.1').empty?
+            vendor_sym = ['FIX_VENDOR']
           end
 
           # chop off all vendor branch versions since HEAD left the branch
           # of course only if we're not (again) on the branch
           rev = rh['1.1.1.1']
           while rev
-            if nomerge_vendor or (trunkdate and rev.date >= trunkdate)
-              rev.action = :vendor
-            else
+            if not trunkrev or rev.date < trunkrev.date
               rev.action = :vendor_merge
+            else
+              rev.action = :vendor
             end
+            rev.syms += vendor_sym
             rev = rh[rev.next]
           end
 
@@ -207,26 +224,77 @@ class Repo
           end
         end
 
-        # correct the next pointers
-        for br in brevs
-          for rev in br.branches
-            # XXX handle versions on nameless branches
+        if rf.branch and rf.branch != '1.1.1'
+          brsplit = rf.branch.split('.')
+          brrev = brsplit[0..1].join('.')
 
-            pr = br
-            begin
-              rev = rh[rev]
-              nrev = rev.next
-              rev.next = pr.rev
-              if rev.branch == rf.branch
-                rev.action ||= :branch_merge
-              else
-                rev.action ||= :branch
-              end
-              pr = rev
-              rev = nrev
-            end while rev
+          rev = rh[rf.head]
+          while rev.rev != brrev
+            rev.action = :ignore
+            rev = rh[rev.next]
           end
-          br.branches = the_one_and_only_empty_list
+
+          level = 1
+          loop do
+            brrev = brsplit[0..(2 * level)]
+            rev = rev.branches.find {|b| b.rev.split('.')[0..(2 * level)] == brrev}
+            rev = rh[rev]
+
+            break if brrev == rf.branch
+
+            brrev = brsplit[0..(2 * level + 1)].join('.')
+            while rev.rev != brrev
+              rev.action = :branch_merge
+              rev = rh[rev.next]
+            end
+            level += 1
+          end
+
+          while rev
+            rev.action = :branch_merge
+            rev = rh[rev.next]
+          end
+        end
+
+        rh.each_value do |rev|
+          if rev.branch_level > 0
+            # if it misses a name, ignore
+            if rev.syms.empty?
+              rev.action = :ignore
+            else
+              if rev.branch_level == 1
+                rev.branch_from = :TRUNK
+              else
+                # determine the branch we branched from
+                br = rev.rev.split('.')[0..-3]
+                # if we "branched" from the vendor branch
+                # we effectively are branching from trunk
+                if br[0..-2] == ['1', '1', '1']
+                  rev.branch_from = :TRUNK
+                else
+                  br = rh[br.join('.')]
+                  rev.branch_from = br.syms[0]
+                end
+              end
+            end
+            rev.action ||= :branch
+          end
+
+          rev.branches.each do |r|
+            r = rh[r]
+            r.link = rev.rev
+
+            # is it "was added on branch" ...?
+            if r.state == :dead and rev.date == r.date
+              r.action = :ignore
+            end
+          end
+        end
+
+        # it was added on a different branch!
+        rev = rh['1.1']
+        if rev and rev.state == :dead     # don't laugh, some files start with 1.2
+          rev.action = :ignore
         end
 
         rh.delete_if { |k, rev| rev.date < from_date }
@@ -247,13 +315,16 @@ class Repo
     set = Set.new
     for r in @revs
       if not set.empty? and not set[-1].same_set?(r)
-        set.sort! {|a, b| a.file <=> b.file }
+        set.extract_data
 	@sets << set
 	set = Set.new
       end
       set << r
     end
-    @sets << set if set.author
+    if not set.empty?
+      set.extract_data
+      @sets << set
+    end
     @sets.sort! {|a, b| a.date <=> b.date }
 
     @revs = nil
@@ -265,12 +336,23 @@ end
 if $0 == __FILE__
   require 'pp'
 
-  r = Repo.new(ARGV[0], lambda {|m| puts m})
-  r.scan
-  r.aggregate
+  repo = Repo.new(ARGV[0], lambda {|m| puts m})
+  repo.scan
+  repo.aggregate
 
-  r.sets.each do |s|
-    puts "#{s.author} #{s.date} on #{s.branch or "trunk"}"
-    s.each {|r| puts "\t#{r.file} #{r.rev} #{r.state} #{r.action} #{r.branches.join(',')}"}
+  repo.sets.each do |s|
+    print "#{s.author} #{s.date} on "
+    if s.branch
+      print "#{s.branch} branching from #{s.branch_from}"
+    else
+      print "trunk"
+    end
+    if s.ignore
+      print " ignore"
+    end
+    print "\n"
+    s.each {|r| puts "\t#{r.file} #{r.rev} #{r.state} #{r.action} #{r.syms.join(',')}"}
   end
+
+  puts "#{repo.sets.length} sets"
 end
